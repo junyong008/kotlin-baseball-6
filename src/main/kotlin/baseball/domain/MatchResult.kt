59236011453840affca04c56feb82ac703005480
junyong <junyong008@gmail.com>
2023-12-07 21:52:41 +0900

    feat: 비교 결과가 모두 스트라이크인지 확인하는 기능 추가

--- src/main/kotlin/baseball/domain/MatchResult.kt
+++ src/main/kotlin/baseball/domain/MatchResult.kt
@@ -5,6 +5,8 @@ data class MatchResult(
     private val ball: Int,
     private val nothing: Int,
 ) {
+    fun isAllStrike(): Boolean = ball < 1 && nothing < 1
+
     override fun toString(): String = when {
         strike > 0 && ball > 0 -> "${ball}볼 ${strike}스트라이크"
         strike > 0 -> "${strike}스트라이크"
